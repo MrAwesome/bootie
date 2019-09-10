8ae83378ffdc47e090b1fe0664fb8f668af6f18f
Glenn Hope <glenn.alexander.hope@gmail.com>
2019-09-10 04:40:02 -0700

    Deal with Centos 7 age insanity

--- bootstrap_scripts/05_distro_centos.sh
+++ bootstrap_scripts/05_distro_centos.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+# TODO: Don't run when not necessary
+
+yum install -y gcc python36-pip python36-jinja2
+
+# TODO: handle this changing the install location. Insane.
+pip3 install --upgrade pip
+pip install jinja2 ansible
+
