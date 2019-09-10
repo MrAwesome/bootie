5f522fdbe27225b7fcafe7ab12cffaf433bf94b8
Glenn Hope <glenn.alexander.hope@gmail.com>
2019-09-10 02:56:36 -0700

    Fix how vars are being passed into initial setup script.

--- bootstrap_scripts/bootstrap.sh
+++ bootstrap_scripts/bootstrap.sh
@@ -16,7 +16,7 @@ cd ..
 # TODO: Set up the file for this, or pass the password etc on the command line.
 #       Passing from command line seems dubious, but probably 
 #       doesn't leave as much of a dangerous paper trail as putting them in a file.
-ansible-playbook "ansible/initial_setup.yml" --extra-vars="user=${SYSTEM_USER},password=${SYSTEM_PASSWORD},github_username=${GITHUB_USERNAME},github_oauth_token=${GITHUB_OAUTH_TOKEN}"
+ansible-playbook "ansible/initial_setup.yml" --extra-vars="user=${SYSTEM_USER} password=${SYSTEM_PASSWORD} github_username=${GITHUB_USERNAME} github_oauth_token=${GITHUB_OAUTH_TOKEN}"
 ansible-playbook "ansible/local.yml" --extra-vars="user=${SYSTEM_USER}"
 
 # TODO: put all this in a repo
