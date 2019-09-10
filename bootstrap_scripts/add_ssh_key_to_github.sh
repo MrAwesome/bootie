42599f06ee9567fc7f6e26b7f6ac939c9597d6ff
Glenn Hope <glenn.alexander.hope@gmail.com>
2019-09-10 02:51:47 -0700

    Functional GitHub ssh key addition from Ansible, user password setting.

--- bootstrap_scripts/add_ssh_key_to_github.sh
+++ bootstrap_scripts/add_ssh_key_to_github.sh
@@ -2,5 +2,4 @@
 
 set -euo pipefail
 
-# TODO: do this from ansible initial setup
 curl -u "${GITHUB_USERNAME}:${GITHUB_OAUTH_TOKEN}" --data '{"title":"'"$(hostname)"' from command line","key":"'"$(cat ~/.ssh/id_rsa.pub)"'"}' https://api.github.com/user/keys 
